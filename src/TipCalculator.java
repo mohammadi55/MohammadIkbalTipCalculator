dd58e9ecae0a4585e132f7a94b873f46f3f7821d
Mohammad <mohammadi55@nycstudents.net>
2022-10-10 15:59:02 -0400

    tipcalculator

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -3,18 +3,18 @@ public class TipCalculator {
     public static void main(String[] args){
         Scanner scan = new Scanner (System.in);
         System.out.print("Bill: ");
-        double bill = scan.nextDouble();
+        double bill = scan.nextDouble(); //stores a double value to the "bill" variable based on the double the user inputs.
 
         System.out.print("Total number of people: ");
-        int people = scan.nextInt();
+        int people = scan.nextInt(); //stores a int value to the "people" variable for the amount of people
 
         System.out.print("Tip percentage: ");
-        double tip = scan.nextDouble();
+        double tip = scan.nextDouble(); //stores a double value to the "tip" variable to represent percentage.
 
 
-        double tip2 = (bill/people)*(tip/100);
-        tip2 = Math.round(tip2*100.0)/100.0;
-        double tip3= (tip2+bill/people);
+        double tip2 = (bill/people)*(tip/100); //tip2 represents the total tip per person
+        tip2 = Math.round(tip2*100.0)/100.0; //rounds the variable to 2 decimal places
+        double tip3= (tip2+bill/people); //tip 3 represents the total tip amount
         tip3 = Math.round(tip3*100.0)/100.0;
         System.out.println("The total bill is "+bill);
         System.out.println("The total number of people is "+people);
